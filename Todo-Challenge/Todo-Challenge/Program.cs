0c33b2d643595e8e6f2b56b0beab627070905c03
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-05-21 16:27:39 +0100

    feat: add create and view commands

--- Todo-Challenge/Todo-Challenge/Program.cs
+++ Todo-Challenge/Todo-Challenge/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Todo_Challenge;
+
+class Program
+{
+    static void Main()
+    {
+
+        int lastId = 1;
+        List<TodoItem> itemList = [new(1, "feed the ducks", false)];
+
+        while (true)
+        {
+            Console.WriteLine("Available commands:\ncreate\nview\nremove {id}\ncomplete {id}\n\n");
+
+            switch (Console.ReadLine())
+            {
+                case "create":
+                    Console.WriteLine("Enter task description:\n");
+                    string? descriptionInput = Console.ReadLine();
+
+                    string description;
+                    if (descriptionInput == null)
+                    {
+                        description = "";
+                    }
+                    else
+                    {
+                        description = descriptionInput;
+                    }
+
+                    Console.WriteLine("Set as complete?[y/n]:\n");
+                    string? completeInput = Console.ReadLine();
+
+                    var complete = completeInput switch
+                    {
+                        "y" => true,
+                        "n" => false,
+                        _ => false,
+                    };
+                    TodoItem newItem = new(lastId++, description, complete);
+
+                    itemList.Add(newItem);
+
+                    Console.WriteLine("New item added\n\n");
+
+                    break;
+
+                case "view":
+                    foreach (var item in itemList)
+                    {
+                        Console.WriteLine($"Id: {item.id}, Description: {item.description}, Complete: {item.complete}");
+                    }
+                    Console.WriteLine("\n\n");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid command\n\n");
+                    break;
+            }
+        }
+    }
+}
