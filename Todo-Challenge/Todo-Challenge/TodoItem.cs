e223d8ee7074ad6b6acadcb98e6a0bebdb86557b
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-05-21 16:27:21 +0100

    feat: add TodoItem Class

---
+++ Todo-Challenge/Todo-Challenge/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Todo_Challenge;
+
+public class TodoItem(int itemId, string itemDescription, bool itemComplete)
+{
+    public int id = itemId;
+    public bool complete = itemComplete;
+    public string description = itemDescription;
+}
