47d46a153428eb89ff763cee650773fdce4bd454
Lukas <lad_777@hotmail.com>
2024-05-22 13:12:28 +0100

    Added options via text.

--- Todo-Challenge/Todo-Challenge/Program.cs
+++ Todo-Challenge/Todo-Challenge/Program.cs
@@ -6,7 +6,13 @@ namespace ToDoListApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+                Console.WriteLine("Welcome to the To-Do List App!");
+                Console.WriteLine("\nPlease choose an action:");
+                Console.WriteLine("1. Add todo item");
+                Console.WriteLine("2. Complete todo item");
+                Console.WriteLine("3. See all todo items");
+                Console.WriteLine("4. Delete todo item");
+                Console.WriteLine("5. Exit");
         }
     }
 }
