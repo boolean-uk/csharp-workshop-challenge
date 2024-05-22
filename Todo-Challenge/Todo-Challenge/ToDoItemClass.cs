fc8de559591d56daaf8d4961c67dd834a69ee785
Lukas <lad_777@hotmail.com>
2024-05-22 13:19:25 +0100

    Todo item class created

--- Todo-Challenge/Todo-Challenge/ToDoItemClass.cs
+++ Todo-Challenge/Todo-Challenge/ToDoItemClass.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace ToDoListApp
+{
+    public class ToDoItem
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public ToDoItem(string description)
+        {
+            Description = description;
+            IsCompleted = false;
+        }
+    }
+}
