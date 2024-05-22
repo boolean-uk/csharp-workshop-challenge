46ee951c34df03c0a0f45397912a63c892b16666
Lukas <lad_777@hotmail.com>
2024-05-22 13:27:12 +0100

    added case 1

--- Todo-Challenge/Todo-Challenge/Program.cs
+++ Todo-Challenge/Todo-Challenge/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ToDoListApp
 {
@@ -6,6 +7,10 @@ namespace ToDoListApp
     {
         static void Main(string[] args)
         {
+            List<ToDoItem> todoList = new List<ToDoItem>();
+
+            while (true)
+            {
                 Console.WriteLine("Welcome to the To-Do List App!");
                 Console.WriteLine("\nPlease choose an action:");
                 Console.WriteLine("1. Add todo item");
@@ -13,6 +18,22 @@ namespace ToDoListApp
                 Console.WriteLine("3. See all todo items");
                 Console.WriteLine("4. Delete todo item");
                 Console.WriteLine("5. Exit");
+
+            int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Write("Enter task: ");
+                            string description = Console.ReadLine();
+                            ToDoItem newItem = new(description);
+                            todoList.Add(newItem);
+                            Console.WriteLine("Task added.");
+                            break;
+                    }
+                }
+            }
         }
     }
 }
