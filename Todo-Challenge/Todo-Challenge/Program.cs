5b43bc7255866b735f4d53844c2869cd04fb338c
Chloe Zermatten <chloezermatten@gmail.com>
2024-05-22 12:22:00 +0100

    feat: app listens to input

--- Todo-Challenge/Todo-Challenge/Program.cs
+++ Todo-Challenge/Todo-Challenge/Program.cs
@@ -3,3 +3,19 @@ using Todo_Challenge;
 
 new Menu();
 Menu.DisplayOptions();
+
+string? line = Console.ReadLine();
+
+while (line != "exit")
+{
+    switch (line)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+    }
+    line = Console.ReadLine();
+}
