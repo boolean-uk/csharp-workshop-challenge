2990211bdb05607e463d297a09948f5b5818a789
Nazar Tymiv <nazartumiv@gmail.com>
2024-05-24 22:11:52 +0100

    feat: add TodoItem class

---
+++ Todo-Challenge/Todo-Challenge/TodoItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_Challenge
+{
+    internal class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Completed { get; set; } = false;
+
+        public TodoItem? NewTodoItem { get; set; } = null;
+
+        public TodoItem(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Completed = false;
+        }
+        
+    }
+}
