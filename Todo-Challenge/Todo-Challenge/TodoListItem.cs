fc1c75ca3b75b5aae44868bb6473cb1dab884376
Kye <NovusAa12@proton.me>
2024-05-27 15:35:45 +0100

    add TodoListItem class

---
+++ Todo-Challenge/Todo-Challenge/TodoListItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Todo_Challenge
+{
+    internal class TodoListItem
+    {
+        public string Name { get; set; }
+        public bool Completed { get; set; }
+        public TodoListItem(string name, bool completed)
+        {
+            Name = name;
+            Completed = completed;
+        }
+
+        public TodoListItem(string name) : this(name, false)
+        {
+        }
+    }
+}
